54d33179cbee805c0f00cafee06d7ab2ca2bca8f
mtaft904 <mtaft904@gmail.com>
2021-01-16 23:24:27 -0500

    Update codenames.js

--- codenames.js
+++ codenames.js
@@ -5,14 +5,26 @@ function makeRows(rows, cols) {
     let cell = document.createElement("div");
     grid.appendChild(cell).className = "item";
     cell.textContent = "Item " + c;
+    cell.classList.add(assignTeams(c));
+    cell.addEventListener('click', ()=> {
+      cell.classList.add("clicked");
+    });
   };
 };
 
 makeRows(5, 5);
 
+const items = document.querySelectorAll(".item");
+
+function assignTeams(number){
+  if(number%2==0){
+    return "red";
+  }
+  return "blue";
+}
+
 const spymaster = document.getElementById("spymaster");
 spymaster.addEventListener('click', ()=>{
-  const items = document.querySelectorAll(".item");
   items.forEach((item) => {
       item.classList.add("spymaster");
   });
@@ -20,7 +32,6 @@ spymaster.addEventListener('click', ()=>{
 
 const spy = document.getElementById("spy");
 spy.addEventListener('click', ()=>{
-  const items = document.querySelectorAll(".item");
   items.forEach((item) => {
       item.classList.remove("spymaster");
   });
